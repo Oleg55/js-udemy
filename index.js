c3737b41aaa807ac8d4b53f70571e40ec01b8d47
Oleg Shevchenko <oleg.shevchenko@mbunity.com>
2021-08-19 17:36:20 +0300

    arrays and methods

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // callback func - функция которая должна быть выполнена после того как другая 
 // функция завершила свое выполнение 
+// lesson 19
 
 function lernJS(lang, callback) {
    console.log(`I lern: ${lang}`);
@@ -12,7 +13,9 @@ function done() {
 
 lernJS('JavaScript', done); // передаем без груглых скобок
 
-//objects and destruct
+// lesson 20
+
+// objects and destruct
 
 const options = {
     name: 'test',
@@ -54,3 +57,83 @@ console.log(Object.keys(options).length);
 // деструктуризация обьекта - достаем переменные из обьекта
 const { border, bg } = options.colors;
 console.log( border, bg );
+
+// lesson 21
+
+// массивы и методы
+const arr = [ 1, 2, 13 , 4, 15, 6 ];
+arr[99] = 0;
+// свойство length берет индекс последнего элемена + 1 
+// в нашем случае выведет длинну 100 хотя элеметов будет 7
+console.log(arr.length);
+// удаление элемента с конца массива 
+arr.pop();
+console.log(arr);
+
+// добавление в конец массива
+arr.push(10);
+console.log(arr);
+
+// так же есть удаление добавление в начало массива
+// но их редко используют так как изменяюется вся нумерация ключей массива из-за сдвига
+
+// переборы массива
+
+// циклом for (но это старый способ)
+for ( let i = 0; i < arr.length; i++ ) {
+    console.log(arr[i]);
+}
+
+// конструкция for of
+// можно использовать breack и continue
+for (let randomName of arr) {
+    console.log(randomName);
+}
+
+// метод массива forEach его можно более удобно настроить чем 
+// предыдущие 2
+// forEach не меняет сам массив просто перебирает!
+// может принимать в себя 3 аргумента
+// 1 - это тот элемент который  мы перебирам (название любое)
+// 2 - номер по порядку (index "название тоже может быть любое")
+// 3 - ссылка на массив который мы перебираем (arr "тут название массива")
+// все аргументы находятся в так называемой callback функции в нашем случе стрелочной
+
+arr.forEach( (item, index, arr) => {
+    console.log(`${index}: ${item} from array ${arr}`);
+})
+
+// методы для изменения массива
+// arr.map
+// arr.every/some
+// arr.filter
+// arr.reduce
+
+// метод разбития строки на массив split
+const str = 'brr1, arr2, crr3, grr4, prr5';
+// если сплит оставить пустым то по умолчанию будет запятая
+// в сплит можно указывать любой разделитель
+const arrFromStr = str.split(",");
+console.log(arrFromStr);
+
+// метод сортировки
+// сортирует элементы как строки
+console.log(arrFromStr.sort());
+// пример с числами и sort
+const arr2 = [ 1, 2, 13 , 4, 15, 6 ];
+console.log(arr2.sort());
+// результат [ 1, 10, 13, 15, 2, 4, 6, <93 empty items> ]
+// этого мы можем избежать передав функцию в sort
+console.log(arr2.sort(compareNum)); // это работает из за алгоритма работы sort()
+function compareNum( a, b ) {
+    return a - b;
+}
+
+// метод обьеденения массива в строку join
+// разделитель также можно указать
+console.log(arrFromStr.join(";"));
+
+// псевдо массивы отличаются от обычных тем что у них нет методов
+
+
+
